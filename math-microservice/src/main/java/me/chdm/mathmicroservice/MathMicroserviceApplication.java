8517e70a158c4c05fabd4e7eecf2f59c6d9f1c85
dmitriy-chupakhin <ch.dm.dev@gmail.com>
2022-06-07 18:10:34 +0300

    Initial math-microservice

---
+++ math-microservice/src/main/java/me/chdm/mathmicroservice/MathMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package me.chdm.mathmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathMicroserviceApplication.class, args);
+    }
+
+}
---
+++ math-microservice/src/test/java/me/chdm/mathmicroservice/MathMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chdm.mathmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
