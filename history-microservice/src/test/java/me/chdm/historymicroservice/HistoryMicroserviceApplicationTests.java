b47d04827bf871f66b4df49e580db8df3b8036f3
dmitriy-chupakhin <ch.dm.dev@gmail.com>
2022-06-08 01:13:24 +0300

    init history-microservice

---
+++ history-microservice/src/test/java/me/chdm/historymicroservice/HistoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chdm.historymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
