12677f877cbda225d5657a340d42431197234f28
dmitriy-chupakhin <ch.dm.dev@gmail.com>
2022-06-08 02:38:46 +0300

    init discovery-service

---
+++ discovery-microservice/src/test/java/me/chdm/discoverymicroservice/DiscoveryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chdm.discoverymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
