c1edf4073b53ff0797c097af541812ef6daca45f
dmitriy-chupakhin <ch.dm.dev@gmail.com>
2022-06-08 01:05:05 +0300

    added logic to math-microservice

---
+++ math-microservice/src/main/java/me/chdm/mathmicroservice/service/MathServiceImpl.java
@@ -0,0 +1,20 @@
+package me.chdm.mathmicroservice.service;
+
+import me.chdm.mathmicroservice.model.Question;
+import me.chdm.mathmicroservice.service.api.MathService;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class MathServiceImpl implements MathService {
+    private final Random random = new Random();
+
+    @Override
+    public Question getRandomQuestion() {
+        int max = 10;
+        int a = random.nextInt(max);
+        int b = random.nextInt(max);
+        return Question.builder().question(String.format("%d + %d = ?", a, b)).answer(String.valueOf(a + b)).build();
+    }
+}
